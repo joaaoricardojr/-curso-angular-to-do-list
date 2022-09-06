396bfbcb9c6c045582b8b8d8feb73c978019b00d
joaaoricardojr <joaoricardo.rsj@gmail.com>
2022-09-06 16:33:21 -0300

    implementar o deletar e o adicionar task

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -11,8 +11,7 @@ import { TaskList } from '../../model/task-list';
 export class TodoListComponent implements OnInit {
 
   public taskList: Array<TaskList> = [
-    {task: "Minha nova Task", checked: true},
-    {task: "Minha nova Task 2", checked: false}
+
   ];
 
   constructor() { }
@@ -20,4 +19,20 @@ export class TodoListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public setEmmitTaskList(event: string){
+    this.taskList.push({ task: event, checked: false });
+  }
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja, realmente, apagar tudo?");
+
+    if(confirm){
+      this.taskList = [];
+    }
+
+  }
 }
